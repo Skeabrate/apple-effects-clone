48f75e4f876797ea4484fa000c6ce7edf47ccfad
unknown <sebastianswiecz458@gmail.com>
2022-05-11 13:01:35 +0200

    Prevent scroll v3

--- src/assets/styles/index/index.styles.tsx
+++ src/assets/styles/index/index.styles.tsx
@@ -5,13 +5,23 @@ export const StyledFirstSection = styled.section`
   background-repeat: no-repeat;
   background-size: contain;
   width: 100%;
-  height: 120vh;
+  height: 180vh;
   position: relative;
 
   &::after {
     content: '';
     position: absolute;
     top: 190px;
+    left: 35%;
+    width: 200px;
+    height: 300px;
+    background: darkgreen;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 190px;
     left: 35%;
     width: 200px;
     height: 300px;
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -33,9 +33,8 @@ const Navigation = () => {
       document.body.style.height = '100%';
       document.getElementsByTagName('html')[0].style.overflow = 'hidden'; */
       document.body.style.cssText = 'overflow:hidden !important; height:100vh !important';
-      /* document.body.style.cssText = ''; */
-      /* document.getElementsByTagName('html')[0].style.cssText = 'overflow:hidden !important';
-      document.getElementsByTagName('html')[0].style.cssText = 'height:100vh !important'; */
+      document.getElementsByTagName('html')[0].style.cssText =
+        'overflow:hidden !important; height:100vh !important';
     }
     setToggle(!toggle);
   };
