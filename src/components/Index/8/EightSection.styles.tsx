623fb89bb5b1678194c945d4191332eb0fbb0844
unknown <sebastianswiecz458@gmail.com>
2022-05-30 18:25:08 +0200

    Eight section finished tablet styles

--- src/components/Index/8/EightSection.styles.tsx
+++ src/components/Index/8/EightSection.styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface Props {
   $isAlone?: boolean;
@@ -9,17 +9,6 @@ export const Wrapper = styled.section`
   width: 100%;
   background-color: black;
   color: white;
-
-  div {
-    margin: 0 auto;
-  }
-`;
-
-export const StyledArticle = styled.article`
-  div:first-child {
-    max-width: 440px;
-    padding: 60px 6vw;
-  }
 
   h3 {
     font-size: 3.2rem;
@@ -33,12 +22,186 @@ export const StyledArticle = styled.article`
     padding-block: 10px;
   }
 
-  span {
-    display: block;
+  ${({ theme }) => theme.mq.tablet} {
+    padding-top: 70px;
+
+    p {
+      font-size: 1.9rem;
+    }
+
+    h3 {
+      font-size: ${({ theme }) => theme.fontSize.headingTablet};
+    }
+  }
+
+  ${({ theme }) => theme.mq.desktop} {
+    padding-top: 140px;
   }
 `;
 
-export const StyledImage = styled.div<Props>`
-  max-width: ${({ $isSmall }) => ($isSmall ? '400px' : '500px')};
-  padding-top: ${({ $isAlone }) => ($isAlone ? '30px' : '0')};
+const articleHelper = css`
+  display: flex;
+  flex-direction: column;
+  max-width: 460px;
+  margin: 0 auto;
+
+  div {
+    padding: 60px 6vw;
+  }
+
+  span {
+    display: block;
+  }
+
+  img {
+    align-self: center;
+    object-fit: contain;
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    flex-direction: row;
+    max-width: 900px;
+  }
+`;
+
+const bigImage = css`
+  max-width: 440px;
+  height: auto;
+  object-fit: contain;
+
+  ${({ theme }) => theme.mq.tablet} {
+    max-width: 500px;
+  }
+`;
+
+const smallImage = css`
+  max-width: 350px;
+  height: auto;
+
+  ${({ theme }) => theme.mq.tablet} {
+    max-width: 480px;
+  }
+`;
+
+export const StyledFirstArticle = styled.article`
+  ${articleHelper};
+
+  p {
+    margin-top: 40px;
+  }
+
+  img {
+    ${bigImage};
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    flex-direction: row-reverse;
+
+    img {
+      z-index: 2;
+    }
+
+    div {
+      min-width: 450px;
+      padding: 10px 20px 0 50px;
+    }
+  }
+`;
+
+export const StyledSecondArticle = styled.article`
+  ${articleHelper};
+
+  img {
+    ${smallImage};
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    width: fit-content;
+
+    div {
+			min-width: 290px
+      max-width: 350px;
+      padding: 80px 100px 80px 30px;
+    }
+
+    img {
+      margin-top: -180px;
+      z-index: 1;
+      border: 1px solid #1d1d1d;
+      border-right: unset;
+    }
+  }
+`;
+
+export const StyledThirdArticle = styled.article`
+  ${articleHelper};
+
+  img {
+    ${bigImage};
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    flex-direction: row-reverse;
+
+    div {
+      min-width: 360px;
+      padding: 100px 20px 0 50px;
+    }
+
+    img {
+      z-index: 0;
+      margin-top: -50px;
+    }
+  }
+`;
+
+export const StyledFourthArticle = styled.article`
+  ${articleHelper};
+
+  img {
+    ${bigImage};
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    flex-direction: row-reverse;
+
+    div {
+      min-width: 350px;
+      max-width: 350px;
+      padding: 120px 20px 0 50px;
+    }
+
+    img {
+      z-index: 2;
+      margin-top: -80px;
+    }
+  }
+`;
+
+export const StyledFifthArticle = styled.article`
+  ${articleHelper};
+`;
+
+export const StyledAloneImage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    margin-top: 40px;
+    max-width: 400px;
+    height: auto;
+  }
+
+  ${({ theme }) => theme.mq.tablet} {
+    position: relative;
+    left: 250px;
+
+    img {
+      margin-top: -160px;
+      max-width: 700px;
+      height: auto;
+      z-index: 1;
+    }
+  }
 `;
--- src/components/Index/8/EightSection.tsx
+++ src/components/Index/8/EightSection.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Heading from 'components/Heading/Heading';
-import Image from 'next/image';
-import { StyledArticle, StyledImage, Wrapper } from './EightSection.styles';
+import {
+  StyledAloneImage,
+  StyledFifthArticle,
+  StyledFirstArticle,
+  StyledFourthArticle,
+  StyledSecondArticle,
+  StyledThirdArticle,
+  Wrapper,
+} from './EightSection.styles';
 
 const EightSection = () => {
   return (
     <Wrapper>
-      <StyledArticle>
+      <StyledFirstArticle>
         <div>
           <Heading>
             Bring on <span>the night.</span>
           </Heading>
-          <p style={{ padding: '50px 0 0' }}>
+          <p>
             iPhone 13 Pro was made for low light. The Wide camera adds a wider aperture and our
             largest sensor yet — and it leverages the LiDAR scanner for Night mode portraits. Ultra
             Wide gets a wider aperture, a faster sensor and all-new autofocus. And Telephoto now has
@@ -19,90 +26,76 @@ const EightSection = () => {
           </p>
         </div>
 
-        <StyledImage>
-          <Image
-            src='/images/night_mode_photography_1__q70ptc5ot9u2_large.jpg'
-            height='902'
-            width='676'
-            alt=''
-            aria-hidden='true'
-            layout='responsive'
-          />
-        </StyledImage>
-      </StyledArticle>
+        <img
+          src='/images/night_mode_photography_1__q70ptc5ot9u2_large.jpg'
+          height='902'
+          width='676'
+          alt='Bring on the night.'
+        />
+      </StyledFirstArticle>
 
-      <StyledArticle>
+      <StyledSecondArticle>
         <div>
           <p>The Wide camera captures up to</p>
           <h3>2.2x more light</h3>
           <p>for better photos and videos</p>
         </div>
 
-        <StyledImage $isSmall>
-          <Image
-            src='/images/night_mode_photography_2__cbzts548jxyu_large.jpg'
-            height='829'
-            width='621'
-            alt=''
-            aria-hidden='true'
-          />
-        </StyledImage>
-      </StyledArticle>
+        <img
+          src='/images/night_mode_photography_2__cbzts548jxyu_large.jpg'
+          height='829'
+          width='621'
+          alt='2.2x more light'
+        />
+      </StyledSecondArticle>
 
-      <StyledArticle>
+      <StyledThirdArticle>
         <div>
           <p>The Ultra Wide camera captures</p>
           <h3>92% more light</h3>
           <p>for better photos and videos</p>
         </div>
 
-        <StyledImage>
-          <Image
-            src='/images/night_mode_photography_3__ciuu9113qhea_large.jpg'
-            height='939'
-            width='676'
-            alt=''
-            aria-hidden='true'
-          />
-        </StyledImage>
-      </StyledArticle>
+        <img
+          src='/images/night_mode_photography_3__ciuu9113qhea_large.jpg'
+          height='939'
+          width='676'
+          alt='92% more light'
+        />
+      </StyledThirdArticle>
 
-      <StyledImage $isAlone $isSmall>
-        <Image
+      <StyledAloneImage>
+        <img
           src='/images/night_mode_photography_4__eshaa1swvdoy_large.jpg'
           height='663'
           width='884'
-          alt=''
-          aria-hidden='true'
+          alt='The Ultra Wide camera captures'
         />
-      </StyledImage>
+      </StyledAloneImage>
 
-      <StyledArticle>
+      <StyledFourthArticle>
         <div>
           <h3>
             Night mode now <span>on every camera</span>
           </h3>
         </div>
 
-        <StyledImage>
-          <Image
-            src='/images/night_mode_photography_5__y26qxwgyqyq2_large.jpg'
-            height='848'
-            width='635'
-            alt=''
-            aria-hidden='true'
-          />
-        </StyledImage>
-      </StyledArticle>
+        <img
+          src='/images/night_mode_photography_5__y26qxwgyqyq2_large.jpg'
+          height='848'
+          width='635'
+          alt='Night mode now on every camera'
+        />
+      </StyledFourthArticle>
 
-      <StyledArticle>
+      <StyledFifthArticle>
         <div>
           <h3>
             Sharper, more <span>detailed photos</span> and videos
           </h3>
           <p>in any light</p>
         </div>
-      </StyledArticle>
+      </StyledFifthArticle>
     </Wrapper>
   );
 };
